63e909997037d784e163b394d464984374c7f937
marcus_oa <marcus.official1@gmail.com>
2021-03-22 20:54:04 +0000

    'Fist Enpoint: Basic Health Check' completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name =
+        req
+            .match_info()
+            .get("name")
+            .unwrap_or("World");
+
+    format!("Hello {}!", &name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
