a26a410e1d53a5690d94edd316ada78a08adc28a
marcus_oa <marcus.official1@gmail.com>
2021-03-23 19:13:21 +0000

    Tidied up tests/lib for random port binding and random port binding tests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 //! main.ls
 
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
     // Bubble up the io::Error if we failed to bind
     // the address
     // Otherwise call .await on our server
-    run()?.await
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 //! tests/health_check.rs
 
+use std::net::TcpListener;
+
 // `actix_rt::test` is the testing equivalent of `actix_web::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
@@ -11,7 +13,7 @@
 #[actix_rt::test]
 async fn health_check_works() {
     // No .await, .no .expect
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     //
@@ -20,7 +22,7 @@ async fn health_check_works() {
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check",&address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -35,8 +37,13 @@ async fn health_check_works() {
 // We are also running tests, so it is not worth it to propagate error:
 // if we fail to perform the required setup we can just panic and crash
 // all the thing.
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind to address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    let server = zero2prod::run(listener).expect("Failed to bind to address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
@@ -44,4 +51,6 @@ fn spawn_app() {
     // New dev dependency - let's add tokio to the party with
     // `cargo add tokio --dev --vers 1`
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
